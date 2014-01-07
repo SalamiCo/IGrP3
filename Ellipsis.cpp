433105e9e4b6039e58cf5dce8a29c05fb5e2cda7
Samuel <samuelmgalan@gmail.com>
2014-01-07 11:16:35 +0100

    Collision with ellipse working.

--- Ellipsis.cpp
+++ Ellipsis.cpp
@@ -26,7 +26,7 @@ bool Ellipsis::intersection2Ball(PV2D p, PV2D v, double& tIn, PV2D& normalIn){
 	//Transformaciones y llamar al de Circle
 	//this es la elipse que hay que transformar en un circulo con radio 1 en el centro
 	Matr scale = Matr::getScaleMatr(1/fx, 1/fy);
-	Matr translate = Matr::getTranslateMatr(tx, ty);
+	Matr translate = Matr::getTranslateMatr(-tx, -ty);
 
 	PV2D pos = (scale.prodMatr(translate)).prodVect(p);
 	PV2D vel = scale.prodVect(v);
